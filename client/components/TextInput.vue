31ae2da26735b33123f3e97c8657089825b3fb61
little-manatee <mail.to.manatee@gmail.com>
2020-10-25 17:05:04 +0700

    lesson-16-validating-with-vee-validate

--- client/components/TextInput.vue
+++ client/components/TextInput.vue
@@ -1,30 +1,116 @@
 <template>
-    <input
-        type="text"
-        :value="value"
-        :placeholder="placeholder"
-        @input="$emit('input', $event.target.value)"
-        class="w-full text-xs focus:outline-none bg-brown-lightest p-5 text-brown mb-3"
-    >
+	<div>
+		<validation-provider ref="provider" :rules="rules" v-slot="{ errors }">
+			<div class="mb-2">
+				<label :for="id">
+					{{ label }}
+				</label>
+				<input
+					ref="input"
+					:id="id"
+					:placeholder="placeholder"
+					:type="type"
+					:value="value"
+					:disabled="isDisabled"
+					:autocomplete="autocomplete"
+					@input="$emit('input', $event.target.value)"
+					modifier="underbar"
+					class="w-full text-xs focus:outline-none bg-brown-lightest p-5 text-brown"
+				>
+			</div>
+
+			<div v-show="showError && errors[0]" class="mb-2 text-xs text-red-400">
+				{{ errors[0] }}
+			</div>
+		</validation-provider>
+	</div>
 </template>
 
 <script>
     export default {
-        props: {
-            placeholder: {
-                type: String,
-                required: true
-            },
-            type: {
-                type: String,
-                required: false,
-                default: 'text'
-            },
-            value: {
-                type: String,
-                required: false,
-                default: ''
-            }
-        }
+		props: {
+			label: {
+				type: String,
+				required: false,
+			},
+			id: {
+				type: String,
+				required: true,
+			},
+			type: {
+				type: String,
+				required: false,
+				default: "text",
+			},
+			value: {
+				type: String,
+				required: false,
+				default: "",
+			},
+			rules: {
+				type: String,
+				required: false,
+				default: "",
+			},
+			error: {
+				type: String,
+				required: false,
+				default: "",
+			},
+			readonly: {
+				type: Boolean,
+				required: false,
+				default: false,
+			},
+			autocomplete: {
+				type: String,
+				required: false,
+			},
+			placeholder: {
+				type: String,
+				required: false,
+			},
+			showError: {
+				type: Boolean,
+				required: false,
+				default: true,
+			},
+		},
+		computed: {},
+		data: () => ({
+			isDisabled: false,
+		}),
+		methods: {
+			focus() {
+				this.$refs.input.focus();
+			},
+		},
+		watch: {
+			error: function(newVal, oldVal) {
+				if (!newVal) {
+					return;
+				}
+				this.$refs.provider.applyResult({
+					errors: [this.error],
+					valid: false,
+					failedRules: {},
+				});
+			},
+		},
+		mounted() {
+			if (this.autocomplete == "off") {
+				if (this.autocomplete == "off") {
+					console.log( "off");
+					this.$refs.input.setAttribute("readonly", "readonly");
+				}
+				setTimeout(() => {
+					if (this.readonly) {
+						console.log('Mounted x')
+					} else {
+						this.$refs.input.removeAttribute("readonly");
+					}
+				}, 500);
+			}
+		},
     }
 </script>
--- client/pages/Register.vue
+++ client/pages/Register.vue
@@ -1,41 +1,77 @@
 <template>
-    <div class="container my-16 w-full mx-auto">
-        <div class="max-w-sm mx-auto">
-            <h2 class="text-center text-gold">Register</h2>
+	<div class="container my-16 w-full mx-auto">
+		<div class="max-w-sm mx-auto">
+			<h1 class="text-center text-gold text-2xl mt-6">
+				Register
+			</h1>
 
-            <div class="w-full bg-white shadow-md mt-5 rounded-sm p-12">
-                <text-input
-                    :value="model.name"
-                    v-model="model.name"
-                    placeholder="Enter your name"
-                />
-                <text-input
-                    :value="model.email"
-                    v-model="model.email"
-                    placeholder="Enter your email"
-                />
-                <text-input
-                    type="password"
-                    :value="model.password"
-                    v-model="model.password"
-                    placeholder="Enter your password"
-                />
-                <button class="w-full mt-3 text-sm py-5 bg-emerald text-white rounded-sm focus:outline-none hover:bg-emerald-light">
-                    Sign Up
-                </button>
+            <div class="w-full p-6 bg-white shadow-md mt-5 rounded-sm p-12">
+				<validation-observer ref="observer" v-slot="{ invalid }" tag="form" @submit.prevent="submit()">
+					<text-input
+						id="RegisterUsername"
+						v-model="model.name"
+						placeholder="Fullname"
+						type="text"
+						rules="required"
+						autocomplete="off"
+						:error="errors['name']"
+					/>
+					<text-input
+						id="RegisterEmail"
+						v-model="model.email"
+						placeholder="Email"
+						type="text"
+						rules="required|email"
+						autocomplete="off"
+						:error="errors['email']"
+					/>
+					<text-input
+						id="RegisterPassword"
+						v-model="model.password"
+						placeholder="Password"
+						type="password"
+						rules="required|min:6"
+						autocomplete="off"
+						:error="errors['password']"
+					/>
+					<div v-show="errors['error']" class="pb-1 w-full text-center">
+						<span class="text-red-400 text-xs font-normal px-2">
+							{{ errors["error"] }}
+						</span>
+					</div>
+					<button
+						class="w-full mt-3 text-sm py-5 bg-emerald text-white rounded-sm focus:outline-none hover:bg-emerald-light"
+					>
+						Sign Up
+					</button>
+				</validation-observer>
             </div>
         </div>
     </div>
 </template> 
 
 <script>
+	import formMixin from "@mixins/form";
     export default {
-        data: () => ({
-            model: {
-                name: '',
-                email: '',
-                password: ''
+		mixins: [formMixin],
+		data: () => ({
+			errors: {},
+			model: {
+				name: "",
+				email: "",
+				password: "",
+			},
+		}),
+		methods: {
+            submit() {
+				this.$refs.observer.validate().then((isValid) => {
+					if (!isValid) {
+                        console.log('some invalid')
+						return;
+                    }
+                    console.log('All valid')
+                })
             }
-        })
+        },
     }
 </script>
