1f363068133b2109cd122139d268220e43e5939c
little-manatee <mail.to.manatee@gmail.com>
2020-10-26 17:12:11 +0700

    lesson-20-sending-account-confirmation-mails

--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
+    url: process.env.APP_URL || 'http://localhost:3000',
     databaseUrl:
         process.env.DATABASE_URL ||
-        'mongodb://localhost:27017/ztudy_mevn_mongo',
+        'mongodb://localhost:27017/ztudy_mevn_mongo'
 }
