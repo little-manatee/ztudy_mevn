b65adc9e4adefae97ee4bf0910706eba7448db53
little-manatee <mail.to.manatee@gmail.com>
2020-10-28 14:51:16 +0700

    lesson-31-validating-forgot-password

--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -2,12 +2,13 @@ import { Router } from 'express'
 import authController from '@controllers/v1/auth.controller'
 import loginValidator from '@svalidators/login'
 import registerValidator from '@svalidators/register'
+import forgotPasswordValidator from '@svalidators/forgot-password'
 
 const authRouter = new Router()
 
 authRouter.post('/login', authController.login)
 authRouter.post('/login', loginValidator, authController.login)
 authRouter.post('/register', registerValidator, authController.register)
-authRouter.post('/passwords/email', authController.forgotPassword)
+authRouter.post('/passwords/email', forgotPasswordValidator, authController.forgotPassword)
 
 export default authRouter
